844ab7b7df23c8bbfe8690bfeafe3f8d888d2db9
Keun young Kim <kky0317@gmail.com>
2019-08-21 15:39:03 +0900

    scheduler, error handling

---
+++ main/ErrorHandling/retryWhen.playground/Contents.swift
@@ -0,0 +1,63 @@
+//
+//  Copyright (c) 2019 KxCoding <kky0317@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+import RxSwift
+
+/*:
+ # retryWhen
+ */
+
+let bag = DisposeBag()
+
+enum MyError: Error {
+   case error
+}
+
+var attempts = 1
+
+let source = Observable<Int>.create { observer in
+   let currentAttempts = attempts
+   print("START #\(currentAttempts)")
+   
+   if attempts < 3 {
+      observer.onError(MyError.error)
+      attempts += 1
+   }
+   
+   observer.onNext(1)
+   observer.onNext(2)
+   observer.onCompleted()
+   
+   return Disposables.create {
+      print("END #\(currentAttempts)")
+   }
+}
+
+let trigger = PublishSubject<Void>()
+
+source
+   .subscribe { print($0) }
+   .disposed(by: bag)
+
+
+
