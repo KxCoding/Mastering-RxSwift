a8071485860ab5e29a74569c2ba6eca381b5b52f
Keun young Kim <kky0317@gmail.com>
2019-10-21 01:45:55 +0900

    multicast, publish, replay, refCount, share

---
+++ main/SharingSubscription/share.playground/Contents.swift
@@ -0,0 +1,51 @@
+//
+//  Copyright (c) 2019 KxCoding <kky0317@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+import RxSwift
+
+/*:
+ # share
+ */
+
+let bag = DisposeBag()
+let source = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance).debug()
+
+let observer1 = source
+   .subscribe { print("🔵", $0) }
+
+let observer2 = source
+   .delaySubscription(.seconds(3), scheduler: MainScheduler.instance)
+   .subscribe { print("🔴", $0) }
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+   observer1.dispose()
+   observer2.dispose()
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+   let observer3 = source.subscribe { print("⚫️", $0) }
+
+   DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      observer3.dispose()
+   }
+}
